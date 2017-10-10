6c6c8f35a85540da6743bebef1a925b41302f279
xuh <hayley.xu@york.ca>
2017-10-10 10:59:41 -0400

    Script for tables and views added from dev for support documentation.

---
+++ table_scripts/sequence.sql
@@ -0,0 +1,168 @@
+--------------------------------------------------------
+--  File created - Tuesday-October-10-2017   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence STP_ACTIVITIES_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_ACTIVITIES_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_AREA_FORESTERS_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_AREA_FORESTERS_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_CAPITAL_PROJECTS_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_CAPITAL_PROJECTS_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_COMMENTS_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_COMMENTS_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 342 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_CONTRACT_DETAIL_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_CONTRACT_DETAIL_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 441 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_CONTRACT_PLANTING_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_CONTRACT_PLANTING_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_CONTRACT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_CONTRACT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 381 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_CONTRACTOR_UPLOAD_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_CONTRACTOR_UPLOAD_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 12590 NOCACHE  NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_DEF_LIST_AUTO_ID
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_DEF_LIST_AUTO_ID"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 31161 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_EMAIL_TEMPLATE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_EMAIL_TEMPLATE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_EW_PAY_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_EW_PAY_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_EW_PAYMENT_DETAIL_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_EW_PAYMENT_DETAIL_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_EW_PAYMENT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_EW_PAYMENT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_EW_PAYMENT_SEQ1
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_EW_PAYMENT_SEQ1"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_INSPECTORS_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_INSPECTORS_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_MARK_TYPE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_MARK_TYPE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_MARKING_LOCATION_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_MARKING_LOCATION_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_MUNICIPALITY_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_MUNICIPALITY_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_NURSERY_INSPECTION_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_NURSERY_INSPECTION_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 912 NOCACHE  NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_NURSERY_TYPE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_NURSERY_TYPE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_PLANT_TYPE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_PLANT_TYPE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_PRICE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_PRICE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 841 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_ROW_ITEM_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_ROW_ITEM_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 385 NOCACHE  NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_SPECIES_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_SPECIES_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_STOCK_TYPE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_STOCK_TYPE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_STUMP_SIZE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_STUMP_SIZE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_TAG_COLOR_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_TAG_COLOR_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_TRANSP_DIS_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_TRANSP_DIS_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_TREE_PLANTING_DETAIL_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_TREE_PLANTING_DETAIL_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 81 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_WA_DETAIL_ID_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_WA_DETAIL_ID_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 701 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_WA_SAVE_SEQ_ID
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_WA_SAVE_SEQ_ID"  MINVALUE 0 MAXVALUE 9999999999999 INCREMENT BY 1 START WITH 50284 NOCACHE  NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_WATERING_ITEM_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_WATERING_ITEM_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
+--------------------------------------------------------
+--  DDL for Sequence STP_WRNTY_INSPECTION_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "BSMART_DATA"."STP_WRNTY_INSPECTION_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 301 CACHE 20 NOORDER  NOCYCLE  NOPARTITION ;
